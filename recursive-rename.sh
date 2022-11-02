dfd660ca26f37aba92d200accacb17a40a791f00
Rohan Hope <rohanhope35@gmail.com>
2022-11-02 23:59:04 +1100

    Change sed regex to change globally in string

--- recursive-rename.sh
+++ recursive-rename.sh
@@ -6,7 +6,7 @@ substitution="$2";
 if [[ "$pattern" == *"|"* ]]
 then
 	echo "Warn: Converting | to \|.";
-	pattern=`echo "$pattern" | sed 's/|/\\\|/'`;
+	pattern=`echo "$pattern" | sed 's/|/\\\|/g'`;
 fi
 
 if [ -z "$pattern" ]
@@ -15,4 +15,4 @@ then
 	exit;
 fi
 
-find . -depth -iname "*$pattern*" -exec rename -d -v "s/$pattern/$substitution/" "{}" \;
+find . -depth -iname "*$pattern*" -exec rename -d -v "s/$pattern/$substitution/g" "{}" \;
